b3ba02dc66b2ac4e3b611f7366bd33b37f4977ec
yuma-matsui <oshino.iiyo@gmail.com>
2022-03-17 20:03:44 +0900

    カレンダークラスの修正

--- 02.calendar/cls/calendar.rb
+++ 02.calendar/cls/calendar.rb
@@ -3,7 +3,6 @@ require 'date'
 class Calendar
   def initialize(month, year)
     @today = Date.today
-    @dates = []
     @month = month || @today.month
     @year = year || @today.year
     print_header
@@ -12,48 +11,39 @@ class Calendar
   end
 
   private
-    # 表示するカレンダー用の配列を作成する
-    def calendar_init
-      endof_month = Date.new(@year, @month, -1)
-      endof_month.day.times do |i|
-        @dates.push(Date.new(@year, @month, i + 1))
-      end
-    end
 
-    # 見出しの年月表示用
-    def print_header
-      puts "#{@month}月 #{@year}".center(28)
+  # 見出しの年月表示用
+  def print_header
+    puts "#{@month}月 #{@year}".center(28)
+  end
+  
+  # 見出しの曜日表示用
+  def print_week
+    week = [' 日 ', ' 月 ', ' 火 ', ' 水 ', ' 木 ', ' 金 ', ' 土 ']
+    puts "#{week.join}"
+  end
+  
+  # カレンダーの出力
+  def print_calendar
+    (Date.new(@year, @month, 1)..Date.new(@year, @month, -1)).each do |d|
+      str = insert_space(d)
+      # 月の指定が今月の場合の今日の日付の出力色反転
+      print d == @today ? "\e[47m\e[30m#{str}\e[0m" : str
+      # 曜日が土曜日の時の改行
+      puts if d.wday == 6
     end
-
-    # 見出しの曜日表示用
-    def print_week
-      week = ["日", "月", "火", "水", "木", "金", "土"]
-      week.each_index do |i|
-        print " #{week[i]} "
-        puts "\n" if i == 6
-      end
-    end
-
-    # カレンダーの出力
-    def print_calendar
-      calendar_init
-      @dates.each do |d|
-        str = insert_space(d)
-        # 月の指定が今月の場合の今日の日付の出力色反転
-        print d == @today ? "\e[47m\e[30m#{str}\e[0m" : str
-        # 曜日が土曜日の時の改行
-        print "\n" if d.wday == 6
-      end
-    end
-    
-    # カレンダーの各日付の空白挿入処理
+  end
+  
+  # カレンダーの各日付の空白挿入処理
     def insert_space(d)
-      str = "  #{d.day} "
-      if d.wday != 0 && d.day == 1
-        print "    " * d.wday
-      elsif d.day >= 10
-        str = " #{d.day} "
+      # 月の初日が日曜日以外の場合に空白を挿入
+      print "    " * d.wday if (d.wday != 0) && (d.day == 1)
+      
+      str = d.day.to_s
+      if d.day >= 10
+        str.center(4)
+      else
+        str.rjust(3).ljust(4)
       end
-      return str
     end
 end
