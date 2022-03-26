20c43158afe88c77260b8258be755f8500c174b5
yuma-matsui <oshino.iiyo@gmail.com>
2022-03-27 07:48:32 +0900

    オプションクラスの作成

---
+++ 05.ls/option.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'optparse'
+
+class Option
+  OPTIONS = %w[a].freeze
+
+  attr_reader :options
+
+  def initialize
+    @opt = OptionParser.new
+    @options = {}
+    init_options
+    @opt.parse!(ARGV, into: @options)
+  end
+
+  private
+
+  def init_options
+    OPTIONS.each { |opt| @opt.on("-#{opt}") }
+  end
+end
