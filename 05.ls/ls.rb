c2b5c5b649a137f5f0d423d08fb1e2f5620bd706
yuma-matsui <oshino.iiyo@gmail.com>
2022-04-02 20:01:59 +0900

    Effects Moduleの削除とインスタンスメソッドの簡略化

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,20 +1,14 @@
 # frozen_string_literal: true
 
-require_relative 'effects'
-
 class LS
   # 出力時の最大横列数
   MAX_WIDTH = 3
-  OPTIONS = {
-    a: Effects.excludes_secret!,
-    r: Effects.reverse!
-  }.freeze
 
-  def initialize(path, opts)
+  def initialize(path, options)
     path ||= './'
     inspect_path(path) unless path == './'
-    options = init_options(opts)
-    @files = init_files(path, options)
+    @files = Dir.entries(path).sort
+    apply_effects!(options)
   end
 
   def print_files
@@ -37,30 +31,10 @@ class LS
     exit
   end
 
-  # オプションの判定
-  def init_options(opts)
-    options = {
-      a: true,
-      r: false
-    }
-    opts.each do |key, val|
-      options[key] = !options[key] if val
-    end
-    options
-  end
-
-  # 表示ファイルの初期化
-  def init_files(path, options)
-    files = Dir.entries(path).sort
-    apply_effects(files, options)
-  end
-
   # 各オプションに対応した配列操作
-  def apply_effects(files, options)
-    options.each do |key, val|
-      OPTIONS[key].call(files) if val
-    end
-    files
+  def apply_effects!(options)
+    @files.reject! { |f| f.start_with?('.') } unless options[:a]
+    @files.reverse! if options[:r]
   end
 
   # 空白を挿入してファイル名を20文字に統一
