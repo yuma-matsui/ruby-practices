1e5971cbb4185aa64c26399f5a4700210770fd7f
yuma-matsui <oshino.iiyo@gmail.com>
2022-06-04 11:36:24 +0900

    修正#Frameクラスの初期化処理変更, インスタンス変数の追加

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -3,23 +3,36 @@
 require_relative 'shot'
 
 class Frame
-  attr_reader :first_shot, :second_shot, :third_shot, :frame
+  attr_reader :first_shot, :second_shot, :order, :shots
 
-  def initialize(first_shot, second_shot, third_shot = nil)
+  def initialize(index, first_shot, second_shot, third_shot = nil)
     @first_shot = Shot.new(first_shot)
     @second_shot = Shot.new(second_shot)
     @third_shot = Shot.new(third_shot)
-    @frame = init_frame
+    @order = index
+    @shots = init_frame
   end
 
   def score
-    frame.map(&:score).sum
+    shots.map(&:score).sum
+  end
+
+  def last?
+    (shots.size == 3) || (order == 9)
+  end
+
+  def strike?
+    shots.size == 1
+  end
+
+  def spare?
+    (shots.size == 2) && (score == 10)
   end
 
   private
 
   def init_frame
-    return [first_shot, second_shot, third_shot] unless third_shot.mark.nil?
+    return [first_shot, second_shot, @third_shot] unless @third_shot.mark.nil?
 
     if first_shot.mark == 'X'
       [first_shot]
