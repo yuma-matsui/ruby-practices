9c98c842a033726a6687b9e8a78bf95da0eea338
yuma-matsui <oshino.iiyo@gmail.com>
2022-06-12 17:14:02 +0900

    修正#file_infoメソッドの分割と指定されたパスがファイルだった場合の分岐処理追加

--- 08.ls_object/display_with_l.rb
+++ 08.ls_object/display_with_l.rb
@@ -5,7 +5,7 @@ require_relative 'modules/ls'
 
 class LS::DisplayWithL < LS::Display
   def print
-    # 指定されたpathがファイルの場合表示しない
+    # path == Fileの場合表示しない
     puts "total #{total_size}" unless @files.size == 1
     @files.each { |file| puts file_info(file.info) }
   end
@@ -17,8 +17,21 @@ class LS::DisplayWithL < LS::Display
   end
 
   def file_info(info)
-    str = "#{info[:ftype]}#{info[:permission]}#{info[:nlink]} #{info[:owner_name]} #{info[:group_name]} #{info[:size]} #{info[:time_stamp]} #{info[:file_name]}"
+    str = "#{first_half_info(info)} #{latter_half_info(info)} #{file_path_or_name(info)}"
     str += " -> #{info[:symlink]}" unless info[:symlink].nil?
     str
+  end
+
+  def first_half_info(info)
+    "#{info[:ftype]}#{info[:permission]}#{info[:nlink]}"
+  end
+
+  def latter_half_info(info)
+    "#{info[:owner_name]} #{info[:group_name]} #{info[:size]} #{info[:time_stamp]}"
+  end
+
+  # path == Fileの場合の分岐のため切り出し
+  def file_path_or_name(info)
+    @files.size == 1 ? info[:path] : info[:file_name]
   end
 end
